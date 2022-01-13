f15f482f526066b19f7b8c386b460d8cd26c1b64
User Name <email@mail.com>
2022-01-13 20:56:38 +0300

    HW23

---
+++ HW23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать таблицу квадратов чисел от 1 до N 
+int n = new Random().Next(1, 11);
+int i = 1;
+while (i < n)
+{
+    int squarenumbers = i * i;
+    Console.WriteLine("Квадрат числа " + i + " = " + squarenumbers);
+    i++;
+}
+
