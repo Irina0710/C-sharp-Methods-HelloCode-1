e241016d0f5f6c2712a9f29c0a16b8b93d12ae8a
User Name <email@mail.com>
2022-01-13 21:59:37 +0300

    HW27

---
+++ HW27/Program.cs
@@ -0,0 +1,13 @@
+﻿// 27. Определить количество цифр в числе
+Console.Clear();
+Console.Write("Введите число: ");
+int chislo=int.Parse(Console.ReadLine());
+int i=0;
+while(chislo>0) 
+{
+   i++;
+   chislo/=10;
+}
+Console.WriteLine("Количество цифр: {0}", i);
+return 0;
+
