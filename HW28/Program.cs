2d065122039517379f7f594144afbc0ae36d83ff
User Name <email@mail.com>
2022-01-13 22:05:12 +0300

    HW28

---
+++ HW28/Program.cs
@@ -0,0 +1,12 @@
+﻿// 28. Подсчитать сумму цифр в числе
+Console.Clear();
+Console.Write("Введите число: ");
+int number=int.Parse(Console.ReadLine());
+int i=0;
+while(number>0) 
+{
+   i = i + number % 10;
+   number /= 10;
+}
+Console.WriteLine("Сумма числа: {0}", i);
+return 0;
