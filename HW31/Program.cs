57f5506de46cbf616235d25e7df8cbd83bb5a453
User Name <email@mail.com>
2022-01-17 20:34:30 +0300

    HW31

---
+++ HW31/Program.cs
@@ -0,0 +1,7 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран 
+int[] array = new int [8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 9);
+    Console.WriteLine(array[i]);
+}
