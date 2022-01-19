32111f3d5290a680c696468f56374c76c4054fa6
User Name <email@mail.com>
2022-01-19 20:27:47 +0300

    HW34

---
+++ HW34/Program.cs
@@ -0,0 +1,8 @@
+﻿// 34. Написать программу замену элементов массива на противоположные
+int [] array = new int [6] {8, -3, 16, -6, -1, 0};
+for (int i = 0; i<6; i++)
+{
+    Console.WriteLine(array[i]);
+    array[i] = -array[i];
+    Console.WriteLine(array[i]);
+}
