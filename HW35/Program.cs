75ce34a7af76637bda27e45b079aae1e018a3f91
User Name <email@mail.com>
2022-01-19 20:52:51 +0300

    HW35

---
+++ HW35/Program.cs
@@ -0,0 +1,20 @@
+﻿// // 35. Определить, присутствует ли в заданном массиве, некоторое число 
+int [] array = new int [10];
+for (int i = 0; i< array.Length; i++)
+{
+    array[i] = new Random().Next(0,10);
+    Console.WriteLine(array[i]);
+    Console.WriteLine();
+}
+for (int i = 0; i< array.Length; i++)
+{
+    //ищем цифру пять
+    if (array[i] == 5)
+    {
+     Console.WriteLine($"Цифра 5 найдена под индексом {i}");
+    }
+    else
+    {
+      Console.WriteLine($"Цифра 5 не найдена на этом месте индекса ({i})");  
+    }
+}
