0950813e4d025ebbec9ef41dbb5e0fe13c066663
User Name <email@mail.com>
2022-01-21 19:52:17 +0300

    HW40

---
+++ HW40/Program.cs
@@ -0,0 +1,26 @@
+﻿// 40 В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+double [] Array = {4,6,18,10,12,12,14,16};
+double MaxNumber(double[] Array)
+{
+    double max = Array[0];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] > max) max = Array[i];
+    }
+    return max;
+}
+double MinNumber(double[] Array)
+{
+    double min = Array[0];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] < min) min = Array[i];
+    }
+    return min;
+}
+double a = MaxNumber(Array);
+Console.WriteLine(a);
+double b = MinNumber(Array);
+Console.WriteLine(b);
+Console.WriteLine(a-b);
+
