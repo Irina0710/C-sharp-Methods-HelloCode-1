1395c4da588a88cc7fb579cc25a43b855ea8f3a3
User Name <email@mail.com>
2022-01-25 20:57:46 +0300

    HW42

---
+++ HW42/Program.cs
@@ -0,0 +1,15 @@
+﻿// 42. Определить сколько чисел больше 0 введено с клавиатуры
+int i = 0;
+int count = 0;
+int Number = 3;
+while (i < Number)
+{
+    Console.WriteLine("Please insert a number: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    if (a>0)
+    {
+        count=count+1;
+    }
+    i++;
+}
+Console.WriteLine($"The amount of numbers typed using keyboard higher then 0 equals to {count}");
