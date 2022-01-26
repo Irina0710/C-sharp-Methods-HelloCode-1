57aaf704a82edbba424696783cd51b4d046915cf
User Name <email@mail.com>
2022-01-27 00:49:08 +0300

    HW47

---
+++ HW47/Program.cs
@@ -0,0 +1,9 @@
+﻿// 47. Написать программу копирования массива
+int[] array = new int[10] {2,4,3,5,6,5,4,4,3,6};
+int[] copy = new int[10];
+
+for (int i=0; i <array.Length; i++)
+{
+    copy[i] = array[i];
+    Console.WriteLine (copy[i]);
+}
