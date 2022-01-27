09241bf8a6af869e6281d8e5f743a5e942f15887
User Name <email@mail.com>
2022-01-27 21:14:15 +0300

    HW50

---
+++ HW50/Program.cs
@@ -0,0 +1,27 @@
+﻿// 50. В двумерном массиве n×k заменить четные элементы на противоположные
+int k = 4;
+int n = 4;
+int[,] twoarray = new int [k, n];
+
+{
+    for (int i = 0; i < k; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            twoarray[i,j] = new Random().Next(1, 10);
+            Console.Write (twoarray[i,j]);
+            Console.Write( " ");
+        }
+    }
+}
+for (int i = 0; i < k; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            if (twoarray[i,j]%2 == 0)twoarray[i,j] = -twoarray[i,j];
+            Console.WriteLine();
+            Console.WriteLine(twoarray[i,j]);
+
+        }
+
+    }
