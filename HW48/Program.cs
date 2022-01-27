590f3265712522b6813a2351ccc2c73609a0eb49
User Name <email@mail.com>
2022-01-27 20:33:51 +0300

    HW48

---
+++ HW48/Program.cs
@@ -0,0 +1,14 @@
+﻿// 48. Показать двумерный массив размером m×n заполненный целыми числами
+int m = 5;
+int n = 4;
+int[,] twoarray = new int [m, n];
+for (int i = 0; i < m; ++i) // выводим на экран строку i
+{
+    for (int j = 0; j < n; ++j) // выводим на экран столбец j
+    {
+        twoarray [i,j] = new Random().Next(1,10);
+        Console.Write(twoarray [i,j]);
+        Console.Write( " ");
+    }
+    Console.WriteLine();
+}
