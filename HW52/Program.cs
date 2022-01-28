2119618d0164060bebd9db4d6874cb2ef0a87ebc
Irina0710 <defmail2me@gmail.com>
2022-01-28 17:59:42 +0300

    HW52

---
+++ HW52/Program.cs
@@ -0,0 +1,29 @@
+﻿// 52. В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+int k = 4;
+int n = 4;
+
+int [,] twoarray = new int [k,n];
+
+for (int i = 0; i < k; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            twoarray[i,j] = new Random().Next(1,10);
+            Console.Write (twoarray[i, j]);
+            Console.Write( " ");
+        }
+        Console.WriteLine();
+    }
+for (int i = 0; i < k; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            if (i %2 == 0 && j %2 == 0) twoarray[i,j] = twoarray[i,j] * twoarray[i,j];
+            twoarray[i,j] = new Random().Next(1,10);
+            Console.Write (twoarray[i, j]);
+            Console.Write( " ");
+        }
+        Console.WriteLine();
+    }
+
+
