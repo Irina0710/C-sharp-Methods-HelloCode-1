3357701d3f33e158cd8dbd199c8ab24a8407c31c
Irina0710 <defmail2me@gmail.com>
2022-01-28 16:03:21 +0300

    HW51

---
+++ HW51/Program.cs
@@ -0,0 +1,14 @@
+﻿// // 51. Задать двумерный массив следующим правилом: Aₘₙ = m+n
+int m = 5;
+int n = 5;
+int[,] twoarray = new int [m, n];
+
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            twoarray[i,j] = i + j;
+            Console.Write (twoarray[i, j]);
+        }
+    }
+            Console.WriteLine( " ");
