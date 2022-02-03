f6514636047e748ef281c92d778ad239f7e945c0
Irina0710 <defmail2me@gmail.com>
2022-02-03 22:37:35 +0300

    hw68

---
+++ HW68/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать натуральные числа от M до N, N и M заданы (с помощью рекурсии)
+void PrintNumbers(int n, int i)
+{
+    Console.Write($"{i} ");
+    if (i != n)
+        PrintNumbers (n,i+1);
+} 
+Console.WriteLine("Введите второе число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+int M = Convert.ToInt32(Console.ReadLine());
+PrintNumbers(N, M);
+
