9d1d0513c2d199a6cfebfd457df7cf28d76eef54
Irina0710 <defmail2me@gmail.com>
2022-02-03 22:22:20 +0300

    hw66

---
+++ HW66/Program.cs
@@ -0,0 +1,11 @@
+﻿// 66. Показать натуральные числа от 1 до N, N задано (с помощью рекурсии)
+void PrintNumbers(int n, int i = 1)
+{
+    Console.Write($"{i} ");
+    if (i != n)
+        PrintNumbers (n,i+1);
+} 
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+PrintNumbers(N);
+
