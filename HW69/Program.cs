1af21a8e4f688e1a1c755fba0e5b6ea24ace12df
Irina0710 <defmail2me@gmail.com>
2022-02-03 23:12:12 +0300

    hw69

---
+++ HW69/Program.cs
@@ -0,0 +1,18 @@
+﻿// 69. Найти сумму элементов от M до N, N и M заданы (с помощью рекурсии)
+int sum = 0;
+int Sum(int m,int n)
+{
+    if(m <=n)
+    {
+        return m+Sum(m+1,n);
+    
+    }
+    return 0;
+}
+
+Console.WriteLine("Введите первое число");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int n = Convert.ToInt32(Console.ReadLine());
+Sum(m, n);
+Console.WriteLine(Sum(m, n));
